b3f4828c5715ed52744b60141cae0b07c7d5f628
VittoriaFrau <vittoriafrau1995f@gmail.com>
2022-05-10 14:48:35 +0200

    MRTK plugin and cleaning

---
+++ Assets/EcaRules/Visualization/OnClickScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void provaClick()
+    {
+        Debug.Log("STO CLICCANDO");
+    }
+    
+    public void provaClick2()
+    {
+        Debug.Log("prova click 2");
+    }
+}
