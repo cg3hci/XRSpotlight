f0ad848f227e13a6fc4afe66d1dcd7811bd7a647
VittoriaFrau <vittoriafrau1995f@gmail.com>
2022-06-03 21:10:56 -0400

    EventFinder from GameObject, still issues with receivers

--- Assets/XRSpotlightGUI/EcaEventFinder.cs
+++ Assets/XRSpotlightGUI/EcaEventFinder.cs
@@ -1,80 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Microsoft.MixedReality.Toolkit.UI;
 using UnityEngine;
 using UnityEngine.Events;
+using Object = UnityEngine.Object;
 
 public class EcaEventFinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    class ListenerInfo
+    class UnityEventInfo
     {
-         MethodInfo listener, reinstantor;
-          Component component;
-    }
-    void Start()
-    {
-        GameObject vanxVintage = GameObject.Find("VanxVintage");
-        /*List<InteractableEvent> events = vanxVintage.GetComponent<Interactable>().InteractableEvents;
-        
-        for(int i=0; i<events.Count; i++)
+        private MethodInfo method;
+        private GameObject gameObjectScript;
+        private string typeOfEvent;
+        private string scriptContainingTheEvent;
+        public UnityEventInfo(MethodInfo method, GameObject gameObjectScript, string typeOfEvent, string scriptContainingTheEvent)
         {
-            InteractableEvent ev = events[i];
-            UnityEvent e = ev.Event;
-            int eCount = e.GetPersistentEventCount();
-            for(int j=0;j<eCount;j++)
-            {
-                Object targetE = e.GetPersistentTarget(i);
-                GameObject target = GameObject.Find(targetE.name);
-                string methodNameE = e.GetPersistentMethodName(i);
-                MonoBehaviour[] componentsE = target.GetComponents<MonoBehaviour>();
-                foreach (MonoBehaviour component in componentsE)
-                {
-                    MethodInfo methodInfo = component.GetType().GetMethod(methodNameE);
-                    if (methodInfo != null)
-                    {
-                        Debug.Log("Found method " + methodNameE + " on " + component.GetType().Name);
-                    }
-                }
-            }
+            this.method = method;
+            this.gameObjectScript = gameObjectScript;
+            this.typeOfEvent = typeOfEvent;
+            this.scriptContainingTheEvent = scriptContainingTheEvent;
         }
-        
-        
-        //On click normale
-        //aggiunta listener runtime
-        //vanxVintage.GetComponent<Interactable>().OnClick.AddListener(() => {    Debug.Log("click add listener"); });
-        */
-        
-        //UnityEventBase v = vanxVintage.GetComponent<Interactable>().OnClick.GetPersistentListenerState();
-        
-        UnityEvent onClicko = vanxVintage.GetComponent<Interactable>().OnClick;
-        var r = vanxVintage.GetComponent<Interactable>().GetReceivers<ReceiverBase>();
-        int eventCount = onClicko.GetPersistentEventCount();
-        for (int i = 0; i < eventCount; i++)
-        {
-            Object target = onClicko.GetPersistentTarget(i);
-            GameObject gameObject = GameObject.Find(target.name);
-            string methodName = onClicko.GetPersistentMethodName(i);
-            MonoBehaviour[] components = gameObject.GetComponents<MonoBehaviour>();
-            foreach (MonoBehaviour component in components)
-            {
-                MethodInfo methodInfo = component.GetType().GetMethod(methodName);
-                if (methodInfo != null)
-                {
-                    Debug.Log("Found method " + methodName + " on " + component.GetType().Name);
-                }
-            }
-        }
-        
-        //ricerca dei metodi:
-        
-        
-        //Debug.Log(events.Count);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     
+    void Start()
+    {
+       // FindInteractableEventsByGameObject(GameObject.Find("OggettoProvaVicky"));
+    }
+
+    void FindInteractableEventsByGameObject(GameObject gameObject)
+    {
+        List<UnityEventInfo> unityEventInfos = new List<UnityEventInfo>();
+        
+        //onclick events
+        UnityEvent onClick = gameObject.GetComponent<Interactable>().OnClick;
+        int eventCount = onClick.GetPersistentEventCount();
+        for (int i = 0; i < eventCount; i++)
+        {
+            unityEventInfos=unityEventInfos.Concat(FindEventsInfo(onClick, i)).ToList();
+        }
+        
+        //receivers
+        var receivers = gameObject.GetComponent<Interactable>().GetReceivers<ReceiverBase>();
+        int receiversCount = receivers.Count();
+        for (int i = 0; i < receiversCount; i++)
+        {
+            ReceiverBase receiverBase = receivers.ElementAt(i);
+            if (receiverBase.GetType().Name == "InteractableOnFocusReceiver")
+            {
+                InteractableOnFocusReceiver interactableReceiver = receiverBase as InteractableOnFocusReceiver;
+                if (interactableReceiver.OnFocusOff!=null)
+                {
+                    unityEventInfos=unityEventInfos.Concat(FindEventsInfo(interactableReceiver.OnFocusOff, i, receivers[i].Name)).ToList();
+                }
+                if (interactableReceiver.OnFocusOn!=null)
+                {
+                    unityEventInfos=unityEventInfos.Concat(FindEventsInfo(interactableReceiver.OnFocusOn, i, receivers[i].Name)).ToList();
+                }
+            }
+            // unityEventInfos=unityEventInfos.Concat(FindEventsInfo(receivers[i].Event, i, receivers[i].Name)).ToList();
+        }
+        
+        Debug.Log(unityEventInfos);
+    }
+
+    private List<UnityEventInfo> FindEventsInfo(UnityEvent unityEvent, int index, string eventType="OnClick")
+    {
+        List<UnityEventInfo> result = new List<UnityEventInfo>();
+        Object target = unityEvent.GetPersistentTarget(index);
+        GameObject gameObjectParameter = GameObject.Find(target.name);
+        string methodName = unityEvent.GetPersistentMethodName(index);
+        MonoBehaviour[] components = gameObjectParameter.GetComponents<MonoBehaviour>();
+        foreach (MonoBehaviour component in components)
+        {
+            UnityEventInfo unityEventInfo = FindSingleEventInfo(methodName, component, gameObjectParameter, eventType);
+            if(unityEventInfo != null)
+            {
+                result.Add(unityEventInfo);
+            }
+        }
+
+        return result;
+    }
+
+    private UnityEventInfo FindSingleEventInfo(string methodName, MonoBehaviour component, GameObject gameObjectParameter, string eventType)
+    {
+        MethodInfo methodInfo = component.GetType().GetMethod(methodName);
+        if (methodInfo != null)
+        {
+            return new UnityEventInfo(methodInfo, gameObjectParameter, eventType, component.GetType().Name);
+        }
+        return null;
+    }
 }
